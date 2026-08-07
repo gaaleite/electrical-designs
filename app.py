cf10be261eface49e983bcd988bb1a87a733e9e6
gaaleite <115560069+gaaleite@users.noreply.github.com>
2026-08-07 10:36:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -290,7 +290,6 @@ if "1." in ambiente:
     if linhas_relatorio:
 
 
-
 # ==========================================
 # AMBIENTE 2: DIAGRAMA E LAYOUT (AUTOCAD)
 # ==========================================
